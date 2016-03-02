449b0a34a2817b6b78c84ca075bbae22d47f36bf
JanchuHH <a1500864@myy.haaga-helia.fi>
2016-03-02 13:49:30 +0200

    Tuote.java ja Pizza.java periytyminen

--- Pizzicato_6G1C/src/pizzicato/model/Pizza.java
+++ Pizzicato_6G1C/src/pizzicato/model/Pizza.java
@@ -1,5 +1,52 @@
 package pizzicato.model;
 
-public class Pizza {
+public class Pizza extends Tuote {
+	private int nakyvyys;
+	private String pohja;
+	
+	// Konstruktorit
+	public Pizza(int id, String tyyppi, String nimi, double hinta,
+			int nakyvyys, String pohja) {
+		this.nakyvyys = nakyvyys;
+		this.pohja = pohja;
+	}
 
+	public Pizza() {
+		this.nakyvyys = 0;
+		this.pohja = null;
+	}
+
+	
+	// Getterit ja setterit
+	public int getNakyvyys() {
+		return nakyvyys;
+	}
+
+	public void setNakyvyys(int nakyvyys) {
+		this.nakyvyys = nakyvyys;
+	}
+
+	public String getPohja() {
+		return pohja;
+	}
+
+	public void setPohja(String pohja) {
+		this.pohja = pohja;
+	}
+
+	
+	// toString
+	@Override
+	public String toString() {
+		return "Pizza [nakyvyys=" + nakyvyys + ", pohja=" + pohja + "]";
+	}
+	
+	
+	
+	
+	
+
+	
+	
+	
 }
