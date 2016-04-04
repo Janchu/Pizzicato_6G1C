d9ae5e27ee9693f09053e6f672f6f1db66e38f06
GalloglaichVol3 <a1500869@PAO5003Z30.haagahelia.amk>
2016-04-04 16:24:31 +0300

    Login Servlet, Rekisterointiservlet, kayttaja.java, ym. muutokset.

--- Pizzicato_6G1C/src/pizzicato/control/LoginServlet.java
+++ Pizzicato_6G1C/src/pizzicato/control/LoginServlet.java
@@ -10,29 +10,31 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import pizzicato.model.Kayttaja;
 
 import com.sun.istack.internal.logging.Logger;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private Logger logger = Logger.getLogger(this.getClass());
 	private RequestDispatcher jsp;
-	
+
 	public void init(ServletConfig config) throws ServletException {
 		ServletContext context = config.getServletContext();
 		jsp = context.getRequestDispatcher("/view/listaa-pizzat.jsp");
 	}
-	// Ahalol h
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		logger.debug("doGet()");
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
 		jsp.forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		logger.debug("doGet()");
-		
-		
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
 	}
 
 }
