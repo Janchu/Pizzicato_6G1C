8e1ef0c16dde792f9c2b35e3430c4390f00545df
GarStiver <a1500914@PAO5003Z28.haagahelia.amk>
2016-04-27 15:56:21 +0300

    Asiakaskirjautuminen

---
+++ Pizzicato_6G1C/src/pizzicato/control/AEngListaaPizzatServlet.java
@@ -0,0 +1,60 @@
+package pizzicato.control;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import pizzicato.model.Pizza;
+import pizzicato.model.Tilaus;
+import pizzicato.model.Tilausrivi;
+import pizzicato.model.dao.PizzaDAO;
+
+/**
+ * Servlet implementation class AEngListaaPizzatServlet
+ */
+@WebServlet("/AEngListaaPizzatServlet")
+public class AEngListaaPizzatServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Luodaan PizzaDAO
+		PizzaDAO pizzadao = new PizzaDAO();
+		ArrayList<Pizza> pizzat = pizzadao.findAll();
+		
+		HttpSession session = request.getSession();
+		Tilaus ostoskori = (Tilaus) session.getAttribute("ostoskori");
+		
+		if(ostoskori == null) {
+			ostoskori = new Tilaus();
+			System.out.println(ostoskori);
+		}
+		
+		ArrayList<Tilausrivi> tilausrivit = ostoskori.getTilausrivit();
+		
+		System.out.println(tilausrivit);
+
+		// ArrayList tallennetaan request-olioon jsp:lle vietäväksi
+		request.setAttribute("pizzat", pizzat);
+
+		// Lähetetään jsp:lle
+		String jsp = "/view/aeng-listaa-pizzat.jsp";
+		RequestDispatcher dispatcher = getServletContext()
+				.getRequestDispatcher(jsp);
+		dispatcher.forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
