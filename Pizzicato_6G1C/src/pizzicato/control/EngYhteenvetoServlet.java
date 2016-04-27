00ee3e50ef4de0c57f86e026f81593166190c2d2
WaltzCookie <a1500912@PAO5003Z29.haagahelia.amk>
2016-04-27 17:34:46 +0300

    Asiakkaan näkymiä, enkku näkymiä ja asiakkaan enkku näkymiä. Ihan vituralla juttuja.

--- Pizzicato_6G1C/src/pizzicato/control/EngYhteenvetoServlet.java
+++ Pizzicato_6G1C/src/pizzicato/control/EngYhteenvetoServlet.java
@@ -1,7 +1,6 @@
 package pizzicato.control;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,44 +10,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import pizzicato.model.Pizza;
-import pizzicato.model.Tayte;
+import pizzicato.model.Kayttaja;
 import pizzicato.model.Tilaus;
-import pizzicato.model.Tilausrivi;
-import pizzicato.model.dao.PizzaDAO;
-import pizzicato.model.dao.TayteDAO;
 
 /**
- * Servlet implementation class ListaaPizzatServlet
+ * Servlet implementation class EngYhteenvetoServlet
  */
-@WebServlet("/EngListaaPizzatServlet")
-public class EngListaaPizzatServlet extends HttpServlet {
+@WebServlet("/EngYhteenvetoServlet")
+public class EngYhteenvetoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-		// Luodaan PizzaDAO
-		PizzaDAO pizzadao = new PizzaDAO();
-		ArrayList<Pizza> pizzat = pizzadao.findAll();
-
 		HttpSession session = request.getSession();
-		Tilaus ostoskori = (Tilaus) session.getAttribute("ostoskori");
 
-		if (ostoskori == null) {
-			ostoskori = new Tilaus();
-			System.out.println(ostoskori);
-		}
+		Tilaus tilaus = (Tilaus) session.getAttribute("tilaus");
+		Kayttaja tilaaja = (Kayttaja) session.getAttribute("tilaaja");
 
-		ArrayList<Tilausrivi> tilausrivit = ostoskori.getTilausrivit();
-
-		System.out.println(tilausrivit);
-
-		// ArrayList tallennetaan request-olioon jsp:lle vietäväksi
-		request.setAttribute("pizzat", pizzat);
+		request.setAttribute("tilaus", tilaus);
+		request.setAttribute("tilaaja", tilaaja);
 
 		// Lähetetään jsp:lle
-		String jsp = "/view/eng-listaa-pizzat.jsp";
+		String jsp = "/view/eng-yhteenveto.jsp";
 		RequestDispatcher dispatcher = getServletContext()
 				.getRequestDispatcher(jsp);
 		dispatcher.forward(request, response);
