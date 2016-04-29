cd751a02e9ddcef68106771d58046b4df96fba30
GarStiver <a1500914@PAO5003Z28.haagahelia.amk>
2016-04-29 17:45:48 +0300

    TilausDAO

--- Pizzicato_6G1C/src/pizzicato/model/dao/TilausDAO.java
+++ Pizzicato_6G1C/src/pizzicato/model/dao/TilausDAO.java
@@ -2,7 +2,9 @@ package pizzicato.model.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import pizzicato.model.Kayttaja;
 import pizzicato.model.Tilaus;
@@ -11,6 +13,36 @@ import pizzicato.model.Tilausrivi;
 
 
 public class TilausDAO extends DataAccessObject {
+	
+	public ArrayList<Tilaus> findAll() {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		ArrayList<Tilaus> tilauslista = new ArrayList<Tilaus>();
+		Tilaus tilaus = null;
+		int tilausIdEdellinen = 0;
+		
+		try {
+			conn = getConnection();
+			String sqlSelect = "SELECT tilaus.id, tilaus.tila, tilaus.maksutapa, tilaus.toimitus, tilaus.lisatiedot, tilaus.kayttaja_id, tilaus.yhthinta FROM tilaus;";
+			stmt = conn.prepareStatement(sqlSelect);
+			rs = stmt.executeQuery(sqlSelect);
+			
+			while(rs.next()) {
+				int tilausId = rs.getInt("tilaus.id");
+				if(tilausId != tilausIdEdellinen) {
+					tilaus = readTilaus(rs);
+					tilauslista.add(tilaus);
+				}
+				tilausIdEdellinen = rs.getInt("tilaus.id");
+			}
+		}catch (SQLException e) {
+			throw new RuntimeException(e);
+		}finally {
+			close(rs, stmt, conn);
+		}
+		return tilauslista;
+	}
 
 	public void addTilaus(Tilaus tilaus, Kayttaja tilaaja, Tilausrivi tilausrivi) throws SQLException {
 		
@@ -61,6 +93,24 @@ public class TilausDAO extends DataAccessObject {
 		}
 		
 		
+	}
+	
+	private Tilaus readTilaus(ResultSet rs) {
+		try {
+			int id = rs.getInt("tilaus.id");
+			String tila = rs.getString("tilaus.tila");
+			String maksutapa = rs.getString("tilaus.maksutapa");
+			String toimitus = rs.getString("tilaus.toimitus");
+			String lisatiedot = rs.getString("tilaus.lisatiedot");
+			ArrayList<Tilausrivi> tilausrivit = new ArrayList<Tilausrivi>();
+			double yhthinta = rs.getDouble("tilaus.lisatiedot");
+			
+			return new Tilaus(id, tila, maksutapa, toimitus, lisatiedot, tilausrivit, yhthinta);
+			
+			
+		}catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 	
